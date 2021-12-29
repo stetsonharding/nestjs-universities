edc198eeef03ce4f6c3601c2c980b8b737f23e0e
stetsonharding <stetson2001@msn.com>
2021-12-29 07:11:45 -0800

    Get, Post, and Put HTTP request methods testing.

--- src/universities/universities.controller.ts
+++ src/universities/universities.controller.ts
@@ -1,15 +1,34 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
 import { CreateUniversityDto } from './dto/create-university.dto';
 
 @Controller('universities')
 export class UniversitiesController {
+  //Testing Purposes
+  //Get all Universities
   @Get()
   findAlluniversities(): string {
     return 'Get all items';
   }
 
+  //Get a single University
+  //Takes in an id param, checks to see if an id matches id getting passed in.
+  @Get(':id')
+  findUniversity(@Param() param): string {
+    return `University ${param.id}`;
+  }
+
+  //Testing Purposes
   @Post()
   createUniversity(@Body() _createUniversityDto: CreateUniversityDto): string {
-    return `The City Name is: ${_createUniversityDto.city.name}`;
+    return `The City Name is: ${_createUniversityDto.city.name} id is ${_createUniversityDto.city.id}`;
+  }
+
+  //Testing Purposes
+  @Put(':id')
+  updateUniversity(
+    @Body() _updateUniversityDto: CreateUniversityDto,
+    @Param('id') id,
+  ): string {
+    return `Update ${id} - Name: ${_updateUniversityDto.city.name}`;
   }
 }
